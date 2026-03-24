73425ebe1d2732b7385dd9038282118f032e586c
poaoaooa <e1399179@u.nus.edu>
2026-03-24 14:31:45 +0800

    Revert "Update command_word for UpdateAttendanceCommand"
    This reverts commit e2da4b6a66135b06b2b656fa2f0ef63068d09fc6.

--- src/main/java/seedu/address/logic/commands/UpdateAttendanceCommand.java
+++ src/main/java/seedu/address/logic/commands/UpdateAttendanceCommand.java
@@ -20,7 +20,7 @@ import seedu.address.model.person.WeeklyAttendanceList;
  */
 public class UpdateAttendanceCommand extends Command {
 
-    public static final String COMMAND_WORD = "markattendance";
+    public static final String COMMAND_WORD = "updateattendance";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Updates attendance of a student.\n"
             + "Parameters: INDEX (positive integer) "
